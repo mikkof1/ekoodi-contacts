02afe64acca1e919e597f550a0c16c7115d6acc9
mikkof1 <mikko.laitinen@student.saimia.fi>
2017-06-01 11:27:08 +0300

    Some other data to azure

--- client/src/app/contact/services/contact-storage-interface.ts
+++
@@ -1,9 +0,0 @@
-import {Observable} from "rxjs/Observable";
-import {Contact} from "../contact";
-
-export interface ContactStorageInterface {
-  findContacts(): Observable<Contact[]>;
-  addNewContact(contact: Contact): Observable<any>;
-  editContact(contact: Contact): Observable<any>;
-  deleteContact(contact: Contact): Observable<any>;
-}
--- client/src/environments/environment.api.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  envName:'api',
-  storageUrl:'http://webapicontactekoodi17.azurewebsites.net/api'
-};
