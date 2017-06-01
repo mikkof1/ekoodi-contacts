493fb268949e58d04f464922babe0a531350e339
mikkof1 <mikko.laitinen@student.saimia.fi>
2017-06-01 11:27:08 +0300

    Some other data to azure Update build

--- client/src/app/contact/pipes/contact-name.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-/*import { ContactNamePipe } from './contact-name.pipe';
-
-describe('ContactNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ContactNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
-*/
